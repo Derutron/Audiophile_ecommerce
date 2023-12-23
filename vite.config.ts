6d83068a13c6b02a7aa660918f146fa7a60f5796
Derutron <fidg_1@yahoo.com>
2023-12-23 17:51:39 +0100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), checker({ typescript: true })],
-  base: '/derutron/', // Assuming 'derutron' is your repository name
-})
+  base: '/Audiophile_ecommerce/', // Assuming 'derutron' is your repository name
+  build: {
+    rollupOptions: {
+      external: ["/dist/assets/index--iCSnXwO.js"],
+    },
+  },
+});
